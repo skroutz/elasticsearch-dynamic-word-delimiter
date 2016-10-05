b5e60ffd1f6041339e0eaf43394f71c497a59ed6
Petros Markou <markoupetr@skroutz.gr>
2016-10-05 15:52:26 +0300

    Extract hardcoded values to static variables

--- src/main/java/org/skroutz/elasticsearch/module/WordDelimiterRunnable.java
+++ src/main/java/org/skroutz/elasticsearch/module/WordDelimiterRunnable.java
@@ -10,17 +10,22 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.skroutz.elasticsearch.action.support.WordDelimiterActionListener;
 
 public class WordDelimiterRunnable extends AbstractRunnable {
+	// TODO: Decide on some reasonable value
+	public static final TimeValue REFRESH_INTERVAL = TimeValue.timeValueMinutes(1);
+	public static final String INDEX_NAME = "protected_words";
+	public static final String INDEX_TYPE = "word";
 
 	private final Client client;
 	private final String index;
 	private final long interval;
+	private final String type;
 	private final ESLogger logger = ESLoggerFactory.getLogger(WordDelimiterRunnable.class.getSimpleName());
 
 	public WordDelimiterRunnable(Client client, Settings settings) {
 		this.client = client;
-		this.index = settings.get("plugin.skroutz_word_delimiter.protected_words_index", "protected_words");
-		TimeValue temp = settings.getAsTime("plugin.skroutz_word_delimiter.refresh_interval", TimeValue.timeValueMinutes(1));
-		this.interval = temp.getMillis();
+		this.index = settings.get("plugin.skroutz_word_delimiter.protected_words_index", INDEX_NAME);
+		this.type = settings.get("plugin.skroutz_word_delimiter.protected_words_type", INDEX_TYPE);
+		this.interval = settings.getAsTime("plugin.skroutz_word_delimiter.refresh_interval", REFRESH_INTERVAL).getMillis();
 	}
 
     public void doRun() throws Exception {
