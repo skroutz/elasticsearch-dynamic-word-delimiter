f03df033a6eeeeaab91a5011137789e92a2153aa
Michalis Despotopoulos <mdespoto@skroutz.gr>
2017-04-20 21:49:47 +0300

    Refactor tests

--- src/test/java/org/skroutz/elasticsearch/index/analysis/ProtectedWordsIndexTests.java
+++ src/test/java/org/skroutz/elasticsearch/index/analysis/ProtectedWordsIndexTests.java
@@ -7,44 +7,55 @@ import org.apache.lucene.analysis.Tokenizer;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
 import org.elasticsearch.common.unit.TimeValue;
-import org.elasticsearch.test.ElasticsearchIntegrationTest;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.test.ESIntegTestCase;
 import java.io.StringReader;
 import org.elasticsearch.action.support.WordDelimiterActionListener;
 import org.elasticsearch.plugin.WordDelimiterPlugin;
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope.Scope;
 
-import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
-import static org.elasticsearch.test.ElasticsearchTokenStreamTestCase.assertTokenStreamContents;
+import static org.elasticsearch.common.settings.Settings.builder;
+
+import static org.elasticsearch.test.ESTokenStreamTestCase.assertTokenStreamContents;
 import static org.skroutz.elasticsearch.index.analysis.AnalysisTestsHelper.filterFactory;
 
-import org.junit.Test;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 
 @ThreadLeakScope(Scope.NONE)
-public class ProtectedWordsIndexTests extends ElasticsearchIntegrationTest {
+public class ProtectedWordsIndexTests extends ESIntegTestCase {
   private final WordDelimiterActionListener wordsListener = WordDelimiterActionListener.getInstance();
+  private final static String INDEX_NAME = "protected_words";
+  private final static String TYPE_NAME = "word";
+  private final static String FILTER_NAME = "my_word_delimiter";
+
+  @Override
+  protected Collection<Class<? extends Plugin>> nodePlugins() {
+      return Collections.singleton(WordDelimiterPlugin.class);
+  }
 
   @Override
   protected Settings nodeSettings(int nodeOrdinal) {
-    return settingsBuilder()
-        .put("plugin.types", WordDelimiterPlugin.class.getName())
-        .put("plugin.dynamic_word_delimiter.refresh_interval", "500ms")
+    return builder()
         .put(super.nodeSettings(nodeOrdinal))
+        .put("plugin.dynamic_word_delimiter.refresh_interval", "500ms")
         .build();
   }
 
-  @Test
   public void testAddWordToIndex() throws Exception {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
         .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
-    createIndex("protected_words");
+    createIndex(INDEX_NAME);
     ensureGreen();
-    client().prepareIndex("protected_words", "word", "1").setSource("word", "1tb").execute();
+    client().prepareIndex(INDEX_NAME, TYPE_NAME, "1")
+            .setSource("word", "1tb")
+            .execute();
 
     Thread.sleep(TimeValue.timeValueSeconds(2).getMillis());
 
@@ -53,22 +64,21 @@ public class ProtectedWordsIndexTests extends ElasticsearchIntegrationTest {
 
     String source = "skliros 1tb";
     String[] expected = new String[]{"skliros", "1tb"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
-
-  @Test
+  
   public void testRemoveWordFromIndex() throws Exception {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
         .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
-    createIndex("protected_words");
+    createIndex(INDEX_NAME);
     ensureGreen();
-    client().prepareIndex("protected_words", "word", "1").setSource("word", "1tb").execute();
+    client().prepareIndex(INDEX_NAME, TYPE_NAME, "1").setSource("word", "1tb").execute();
 
     Thread.sleep(TimeValue.timeValueSeconds(2).getMillis());
 
@@ -77,11 +87,13 @@ public class ProtectedWordsIndexTests extends ElasticsearchIntegrationTest {
 
     String source = "skliros 1tb";
     String[] expected = new String[]{"skliros", "1tb"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
 
-    client().prepareDelete("protected_words", "word", "1").execute().actionGet();
+    client().prepareDelete(INDEX_NAME, TYPE_NAME, "1")
+            .execute()
+            .actionGet();
 
     Thread.sleep(TimeValue.timeValueSeconds(2).getMillis());
 
@@ -89,7 +101,8 @@ public class ProtectedWordsIndexTests extends ElasticsearchIntegrationTest {
     assertTrue(protectedWords.isEmpty());
 
     expected = new String[]{"skliros", "1", "tb"};
-    tokenizer = new WhitespaceTokenizer(new StringReader(source));
+    tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
 
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
