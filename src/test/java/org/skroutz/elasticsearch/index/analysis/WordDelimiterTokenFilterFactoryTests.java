534419a70a16f0be5244597d9c5c495be8a34fc9
Michalis Despotopoulos <mdespoto@skroutz.gr>
2017-04-20 21:49:47 +0300

    Refactor tests

--- src/test/java/org/skroutz/elasticsearch/index/analysis/WordDelimiterTokenFilterFactoryTests.java
+++ src/test/java/org/skroutz/elasticsearch/index/analysis/WordDelimiterTokenFilterFactoryTests.java
@@ -21,147 +21,155 @@ package org.skroutz.elasticsearch.index.analysis;
 import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.analysis.TokenFilterFactory;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.WhitespaceTokenizer;
-import org.elasticsearch.test.ElasticsearchTokenStreamTestCase;
-import org.junit.Test;
+import org.elasticsearch.test.ESTokenStreamTestCase;
+
 import java.io.IOException;
 import java.io.StringReader;
 
-import static org.elasticsearch.common.settings.ImmutableSettings.settingsBuilder;
 import static org.skroutz.elasticsearch.index.analysis.AnalysisTestsHelper.filterFactory;
 
-public class WordDelimiterTokenFilterFactoryTests extends ElasticsearchTokenStreamTestCase {
+public class WordDelimiterTokenFilterFactoryTests extends ESTokenStreamTestCase {
 
-  @Test
+  private final static String TYPE_NAME = "dynamic_word_delimiter";
+  private final static String FILTER_NAME = "my_word_delimiter";
+
   public void testDefault() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
     String source = "PowerShot 500-42 wi-fi wi-fi-4000 j2se O'Neil's";
     String[] expected = new String[]{"Power", "Shot", "500", "42", "wi", "fi", "wi", "fi", "4000", "j", "2", "se", "O", "Neil"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
-  @Test
   public void testCatenateWords() throws IOException {
-    Settings indexSettings = settingsBuilder()
-        .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+    Settings indexSettings = Settings.builder()
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.catenate_words", "true")
         .put("index.analysis.filter.my_word_delimiter.generate_word_parts", "false")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
     String source = "PowerShot 500-42 wi-fi wi-fi-4000 j2se O'Neil's";
     String[] expected = new String[]{"PowerShot", "500", "42", "wifi", "wifi", "4000", "j", "2", "se", "ONeil"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
 
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
-  @Test
   public void testCatenateNumbers() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.generate_number_parts", "false")
         .put("index.analysis.filter.my_word_delimiter.catenate_numbers", "true")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
     String source = "PowerShot 500-42 wi-fi wi-fi-4000 j2se O'Neil's";
     String[] expected = new String[]{"Power", "Shot", "50042", "wi", "fi", "wi", "fi", "4000", "j", "2", "se", "O", "Neil"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
-  @Test
   public void testCatenateAll() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.generate_word_parts", "false")
         .put("index.analysis.filter.my_word_delimiter.generate_number_parts", "false")
         .put("index.analysis.filter.my_word_delimiter.catenate_all", "true")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
     String source = "PowerShot 500-42 wi-fi wi-fi-4000 j2se O'Neil's";
     String[] expected = new String[]{"PowerShot", "50042", "wifi", "wifi4000", "j2se", "ONeil"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
-  @Test
   public void testSplitOnCaseChange() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.split_on_case_change", "false")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
     String source = "PowerShot";
     String[] expected = new String[]{"PowerShot"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(new StringReader(source));
+    tokenizer.setReader(new StringReader(source));
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
-  @Test
   public void testPreserveOriginal() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.preserve_original", "true")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
-    String source = "PowerShot 500-42 wi-fi wi-fi-4000 j2se O'Neil's";
+    StringReader source = new StringReader("PowerShot 500-42 wi-fi wi-fi-4000 " +
+            "j2se O'Neil's");
     String[] expected = new String[]{"PowerShot", "Power", "Shot", "500-42", "500", "42", "wi-fi", "wi", "fi", "wi-fi-4000", "wi", "fi", "4000", "j2se", "j", "2", "se", "O'Neil's", "O", "Neil"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(source);
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
-  @Test
   public void testStemEnglishPossessive() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.stem_english_possessive", "false")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
-    String source = "PowerShot 500-42 wi-fi wi-fi-4000 j2se O'Neil's";
+    StringReader source = new StringReader("PowerShot 500-42 wi-fi wi-fi-4000 " +
+            "j2se O'Neil's");
     String[] expected = new String[]{"Power", "Shot", "500", "42", "wi", "fi", "wi", "fi", "4000", "j", "2", "se", "O", "Neil", "s"};
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
-
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+      tokenizer.setReader(source);
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
 
   /** Correct offset order when doing both parts and concatenation: PowerShot is a synonym of Power */
-  @Test
   public void testPartsAndCatenate() throws IOException {
-    Settings indexSettings = settingsBuilder()
+    Settings indexSettings = Settings.builder()
         .put(IndexMetaData.SETTING_VERSION_CREATED, Version.CURRENT)
-        .put("index.analysis.filter.my_word_delimiter.type", "dynamic_word_delimiter")
+        .put(Environment.PATH_HOME_SETTING.getKey(), createTempDir().toString())
+        .put("index.analysis.filter.my_word_delimiter.type", TYPE_NAME)
         .put("index.analysis.filter.my_word_delimiter.catenate_words", "true")
         .put("index.analysis.filter.my_word_delimiter.generate_word_parts", "true")
         .build();
-    TokenFilterFactory filterFactory = filterFactory(indexSettings, "my_word_delimiter");
+    TokenFilterFactory filterFactory = filterFactory(indexSettings, FILTER_NAME);
 
-    String source = "PowerShot";
+    StringReader source = new StringReader("PowerShot");
     String[] expected = new String[]{"Power", "PowerShot", "Shot" };
-    Tokenizer tokenizer = new WhitespaceTokenizer(new StringReader(source));
+    Tokenizer tokenizer = new WhitespaceTokenizer();
+    tokenizer.setReader(source);
 
     assertTokenStreamContents(filterFactory.create(tokenizer), expected);
   }
